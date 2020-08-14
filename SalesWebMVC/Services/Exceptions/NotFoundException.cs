9749dc6da226b63aa05490190f81cfd750f6c29f
jucelio Amaral <jucelioinfo@gmail.com>
2020-08-13 22:00:38 -0300

    Update seller and custom service exception

---
+++ SalesWebMVC/Services/Exceptions/NotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Services.Exceptions
+{
+    public class NotFoundException: ApplicationException
+    {
+        public NotFoundException(string message) : base (message){
+
+        }
+    }
+}
